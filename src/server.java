cd6a9b24ff53a79e3a418d3c4e9b7c6d2b95e271
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-11-06 15:08:41 +0800

    initial added

---
+++ src/server.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class server {
+    File sharedDir;
+    File memberList;
+
+    public server(String dirPath, String listPath){
+        sharedDir=new File(dirPath);
+        memberList=new File(listPath);
+    }
+
+}
