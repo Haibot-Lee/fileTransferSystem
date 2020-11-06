ab03f532b672577d631a3f986fb502a26b97695f
Haibot-Lee <18252125@life.hkbu.edu.hk>
2020-11-06 15:16:21 +0800

    Test area added

--- src/server.java
+++ src/server.java
@@ -4,9 +4,13 @@ public class server {
     File sharedDir;
     File memberList;
 
-    public server(String dirPath, String listPath){
-        sharedDir=new File(dirPath);
-        memberList=new File(listPath);
+    public server(String dirPath, String listPath) {
+        sharedDir = new File(dirPath);
+        memberList = new File(listPath);
     }
 
+    //test area
+    public static void main(String[] args) {
+
+    }
 }
